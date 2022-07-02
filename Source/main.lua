41321ebc66ef02177620a4dd6b879dd2001b2459
jay <jay.cheung@gmail.com>
2022-07-02 18:00:25 -0400

    Can display and animate now!

--- Source/main.lua
+++ Source/main.lua
@@ -2,5 +2,12 @@ import "3BodySystem"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
 local system = ThreeBodySystem()
 
+function pd.update()
+    gfx.sprite.update()
+    system:update()
+end
